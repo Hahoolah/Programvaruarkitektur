bfef07c4acc42f2ed78bb3b9bc67078b0ed3f4d7
jesper-and <jesper.andersson17@outlook.com>
2021-04-20 15:27:41 +0200

    Camera Shake
    created camera shake and added it to the player look script: OBS! cameraShake is currently used by PlayerLook script on camera object. This is not final.

--- Grupprojekt/Assets/scripts/Player/PlayerLook.cs
+++ Grupprojekt/Assets/scripts/Player/PlayerLook.cs
@@ -6,12 +6,14 @@ public class PlayerLook : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform player;
+    CameraShake cameraShake; //camera shake object.
     public float mouseSens = 10;
     private float x = 0, y = 0;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        cameraShake = gameObject.GetComponent<CameraShake>(); // getting the camera shake component from the camera.
     }
 
     // Update is called once per frame
@@ -23,5 +25,10 @@ public class PlayerLook : MonoBehaviour
         x = Mathf.Clamp(x, -90, 90);
         transform.localRotation = Quaternion.Euler(x, 0, 0);
         player.transform.localRotation = Quaternion.Euler(0, y, 0);
+
+        if (Input.GetMouseButtonDown(0)) // if left click, camera shake for 0.15f seconds at magnitude 0.4f.
+        {
+            StartCoroutine(cameraShake.Shake(0.15f, 0.4f));
+        }
     }
 }
